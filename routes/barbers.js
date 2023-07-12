b97bb67b61a8d160ecaff895593d06aab9fb5c80
Danila <danila10danila@mail.ru>
2023-07-12 12:31:51 +0300

    barbers api

--- routes/barbers.js
+++ routes/barbers.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const Barber = require('../models/barber');
 
-/* GET home page. */
+/* GET all barbers. */
 router.get('/', async function(req, res, next) {
     const barbers = await Barber.find();
     res.json(barbers);
