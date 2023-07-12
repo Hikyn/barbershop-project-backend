f0c07b06ee63852ab6035c664e7e36bae42e07d8
Danila <danila10danila@mail.ru>
2023-07-12 12:31:19 +0300

    using .env to hide keys

--- app.js
+++ app.js
@@ -10,15 +10,24 @@ var barbersRouters = require('./routes/barbers');
 var barbershopsRouter = require('./routes/barbershops');
 var customersRouter = require('./routes/customers');
 var timeslotsRouter = require('./routes/timeslots');
+var appointmentRouter = require('./routes/appointments');
 
 var app = express();
 const cors = require("cors")
 app.use(cors())
 
+require('dotenv').config();
+
+const dbUser = process.env.DATABASE_USER;
+const dbPassword = process.env.DATABASE_PASSWORD;
+const dbCluster = process.env.DATABASE_CLUSTER;
+const dbCollection = process.env.DATABASE_COLLECTION;
+
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://theroplay:QUQySLMKQoXejbUD@cluster0.ssqxifh.mongodb.net/barbershop?retryWrites=true&w=majority";
+//console.log(`mongodb+srv://${dbUser}:${dbPassword}@${dbCluster}.ssqxifh.mongodb.net/${dbCollection}?retryWrites=true&w=majority`)
+const mongoDB = `mongodb+srv://${dbUser}:${dbPassword}@${dbCluster}.ssqxifh.mongodb.net/${dbCollection}?retryWrites=true&w=majority`
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -37,15 +46,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/services', servicesRouter);
+app.use('/appointments', appointmentRouter);
 app.use('/barbers', barbersRouters);
 app.use('/barbershops', barbershopsRouter);
 app.use('/customers', customersRouter);
 app.use('/timeslots', timeslotsRouter);
 
+app.get('/appointment', async function(req, res, next) {
+  res.redirect('/appointments')
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
