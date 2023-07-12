773d9ddc0d109f5f9cac511315997e887d9438a3
Danila <danila10danila@mail.ru>
2023-07-12 12:31:58 +0300

    barbershops api

--- routes/barbershops.js
+++ routes/barbershops.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const Barbershop = require('../models/barbershop');
+const Barber = require('../models/barber');
 
-/* GET home page. */
+/* GET all barbershops. */
 router.get('/', async function(req, res, next) {
     const barbershops = await Barbershop.find();
     res.json(barbershops);
+});
+
+/* GET one customer by ID */
+router.get('/:barbershopId', async function(req, res, next) {
+    const barbershop = await Barbershop.findById(req.params.barbershopId);
+    res.json(barbershop);
+  });
+
+/* GET all barbers from barbershop with ID */
+router.get('/:barbershopId/barbers', async function(req, res, next) {
+    const barbershop = await Barbershop.findById(req.params.barbershopId).populate('staff');
+    const barbers = barbershop.staff;
+    res.json(barbers);
 });
 
 module.exports = router;
