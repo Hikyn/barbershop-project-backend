408e316ecc4c23e55249524ff141d89bb0ccc34c
Danila <danila10danila@mail.ru>
2023-07-24 16:21:46 +0300

    error catching for barbershops route

--- routes/barbershops.js
+++ routes/barbershops.js
@@ -2,27 +2,37 @@ var express = require('express');
 var router = express.Router();
 const Barbershop = require('../models/barbershop');
 const Barber = require('../models/barber');
+let tryCatch = require('../utils/tryCatch')
 
 /* GET all barbershops. */
-router.get('/', async function(req, res, next) {
+router.get('/', tryCatch(async function(req, res, next) {
     const barbershops = await Barbershop.find();
+    if (!barbershops) {
+        throw new Error('Barbershops are not found')
+    }
     res.status(200);
     res.json(barbershops);
-});
+}));
 
 /* GET one customer by ID */
-router.get('/:barbershopId', async function(req, res, next) {
+router.get('/:barbershopId', tryCatch(async function(req, res, next) {
     const barbershop = await Barbershop.findById(req.params.barbershopId);
+    if (!barbershop) {
+        throw new Error('Barbershop is not found')
+    }
     res.status(200);
     res.json(barbershop);
-  });
+  }));
 
 /* GET all barbers from barbershop with ID */
-router.get('/:barbershopId/barbers', async function(req, res, next) {
+router.get('/:barbershopId/barbers', tryCatch(async function(req, res, next) {
     const barbershop = await Barbershop.findById(req.params.barbershopId).populate('staff');
+    if (!barbershop) {
+        throw new Error('Barbershop is not found')
+    }
     const barbers = barbershop.staff;
     res.status(200);
     res.json(barbers);
-});
+}));
 
 module.exports = router;
