0bdeba0295d970053da7166e69288b285b4c4454
Danila <danila10danila@mail.ru>
2023-07-24 16:15:40 +0300

    error catching for services route

--- routes/services.js
+++ routes/services.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const Service = require('../models/service');
+let tryCatch = require('../utils/tryCatch')
 
 /* GET all services. */
-router.get('/', async function(req, res, next) {
+router.get('/', tryCatch(async function(req, res, next) {
     const services = await Service.find();
+    if (!appointment) {
+        throw new Error('No services found')
+      }
+      res.status(200);
     res.json(services);
-});
+}));
 
 module.exports = router;
